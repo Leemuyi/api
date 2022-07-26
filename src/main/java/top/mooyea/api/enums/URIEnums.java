2666232f38fb27fa7f52dc7bbeaff226c932adc7
lideyang <lidy@skyvis.com.cn>
2022-07-26 16:53:26 +0800

    Initial commit

---
+++ src/main/java/top/mooyea/api/enums/URIEnums.java
@@ -0,0 +1,31 @@
+package top.mooyea.api.enums;/**
+ * <h1>URIEnums<h1>
+ * <p>Copyright (C), 星期二,26,7月,2022</p>
+ * <br/>
+ * <hr>
+ * <h3>File Info:</h3>
+ * <p>FileName: URIEnums</p>
+ * <p>Author:   mooye</p>
+ * <p>Work_Email： lidy@skyvis.com.cn</p>
+ * <p>E-mail： curtainldy@163.com</p>
+ * <p>Date:     2022/7/26</p>
+ * <p>Description: URI 枚举类</p>
+ * <hr>
+ * <h3>History:</h3>
+ * <hr>
+ * <table>
+ *  <thead>
+ *  <tr><td style='width:100px;' center>Author</td><td style='width:200px;' center>Time</td><td style='width:100px;' center>Version_Number</td><td style='width:100px;' center>Description</td></tr>
+ *  </thead>
+ *  <tbody>
+ *    <tr><td style='width:100px;' center>mooye</td><td style='width:200px;' center>16:51 2022/7/26</td><td style='width:100px;' center>v_0.0.1</td><td style='width:100px;' center>创建</td></tr>
+ *  </tbody>
+ * </table>
+ * <hr>
+ * <br/>
+ *@author mooye
+ */
+
+
+ public enum URIEnums {
+}
